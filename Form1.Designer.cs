47739d70779c5a1d5837f85974000588948bd642
katescha8 <kate.schapo.5@gmail.com>
2023-05-10 20:08:32 +0300

    Добавьте файлы проекта.

---
+++ Form1.Designer.cs
@@ -0,0 +1,134 @@
+﻿namespace Typograf
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.button1 = new System.Windows.Forms.Button();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.BackColor = System.Drawing.SystemColors.ActiveCaption;
+            this.button1.FlatStyle = System.Windows.Forms.FlatStyle.Popup;
+            this.button1.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.button1.Location = new System.Drawing.Point(624, 334);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(153, 82);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "Улучшить";
+            this.button1.UseVisualStyleBackColor = false;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // textBox1
+            // 
+            this.textBox1.BackColor = System.Drawing.SystemColors.InactiveCaption;
+            this.textBox1.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.textBox1.Location = new System.Drawing.Point(33, 281);
+            this.textBox1.MaxLength = 250;
+            this.textBox1.Multiline = true;
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(494, 176);
+            this.textBox1.TabIndex = 1;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label1.Location = new System.Drawing.Point(29, 233);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(148, 24);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "Введите текст:";
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Image = global::Typograf.Properties.Resources.download;
+            this.pictureBox1.Location = new System.Drawing.Point(624, 12);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(255, 201);
+            this.pictureBox1.TabIndex = 3;
+            this.pictureBox1.TabStop = false;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(729, 182);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(44, 13);
+            this.label2.TabIndex = 4;
+            this.label2.Text = "я устал";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Consolas", 26.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label3.Location = new System.Drawing.Point(206, 96);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(170, 41);
+            this.label3.TabIndex = 5;
+            this.label3.Text = "Типограф";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.Window;
+            this.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Zoom;
+            this.ClientSize = new System.Drawing.Size(876, 521);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.button1);
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Типограф";
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,146 @@
+﻿using System;
+using System.ComponentModel;
+using System.Linq;
+using System.Windows.Forms;
+
+namespace Typograf
+{
+    public partial class Form1 : Form
+    {
+        string result = String.Empty;
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            if(!string.IsNullOrEmpty(textBox1.Text))
+            {
+                string text = textBox1.Text;
+
+                result = Punctuation(text);
+                result = PlusMinus(result);
+                result = TechnicalSpecs(result);
+                result = WhiteSpace(result);
+                result = YUX(result);
+                result = Russia(result);
+
+                textBox1.Text = result;
+            }
+            else
+            {
+                MessageBox.Show("Вы ничего не ввели");
+            }
+        }
+        private string WhiteSpace(string words)
+        {
+            string result = words;
+            while(result.Contains("  "))
+            {
+                result = result.Replace("  ", " ");
+            }
+            return result;
+        }
+        private string Punctuation(string words)
+        {
+            string[] punctuation = new string[] { ",", ".", ":", ";", "!", "?"};
+            string[] open = new string[] {"(", "«" };
+            string[] close = new string[] { ")", "»" };
+            string result = words;
+
+            foreach(string c in punctuation)
+            {
+                if (result.Contains(" " + c))
+                {
+                    result = result.Replace(" " + c, c);
+                }
+                if (result.Contains(c))
+                {
+                    result = result.Replace(c, c + " ");
+                }
+            }
+
+            if (result.Contains("—"))
+            {
+                result = result.Replace("—", " — ");
+            }
+
+            foreach(string c in open)
+            {
+                if (result.Contains(c))
+                {
+                    result = result.Replace(c, " " + c);
+                }
+                if (result.Contains(c + " "))
+                {
+                    result = result.Replace(c + " ", c);
+                }
+            }
+            foreach (string c in close)
+            {
+                if (result.Contains(" " + c))
+                {
+                    result = result.Replace(" " + c, c);
+                }
+                if (result.Contains(c))
+                {
+                    result = result.Replace(c, c + " ");
+                }
+            }
+            return result;
+        }
+        private string PlusMinus(string words)
+        {
+            string result = words;
+            while (result.Contains("(+,−)"))
+            {
+                result = result.Replace("(+,−)", "±");
+            }
+            return result;
+        }
+        private string TechnicalSpecs(string words)
+        {
+            string result = words;
+            for (int i = 0; i < words.Length; i++)
+            {
+                if (i + 2 < words.Length && IsNumeric(result[i].ToString()) && IsUnit(result[i + 2].ToString()))
+                {
+                    result = result.Remove(i + 1, 1);
+                    result = result.Insert(i + 1, '\u00A0'.ToString());
+                }
+            }
+            return result;
+        }
+
+        private bool IsNumeric(string word)
+        {
+            double num;
+            return double.TryParse(word, out num);
+        }
+
+        private bool IsUnit(string word)
+        {
+            string[] units = new string[] { "м", "см", "мм", "км", "кг", "г", "А", "мА", "кА", "В", "мВ", "кВ", "Вт", "мВт", "кВт", "Дж", "кДж", "ебар", "Па", "кПа", "бар", "мбар", "мм рт. ст.", "об/мин", "рад/с", "%" };
+            return units.Contains(word);
+        }
+        private string YUX(string words)
+        {
+            string result = words;
+            while (result.Contains(" х"))
+            {
+                result = result.Replace(" х", " йух");
+            }
+            return result;
+        }
+        private string Russia(string words)
+        {
+            string result = words;
+            while (result.Contains("рашка"))
+            {
+                result = result.Replace("рашка", "Великая Россия");
+            }
+            return result;
+        }
+    }
+}
